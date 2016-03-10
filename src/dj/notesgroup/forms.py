41013030ca1334050246a8ef9264d8fddaca3beb
Frederic de Zorzi <fredz@pimentech.fr>
2016-03-10 13:01:23 +0100

    +cancelled

--- src/dj/notesgroup/forms.py
+++ src/dj/notesgroup/forms.py
@@ -203,7 +203,7 @@ class SearchForm(Form):
                                  (1, _("open")),
                                  (2, _("pending")),
                                  (3, _("resolved")),
-                                 (4, _("canceled")),
+                                 (4, _("cancelled")),
                                  (0, _("All")))))
     path = CharField(required=False,
                      widget=Select(choices=(
